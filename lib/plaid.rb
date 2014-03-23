1406a410fe82e0af9777c32dec1616159243d5e4
John Koisch <jk@youcentric.com>
2014-03-23 15:34:24 -0700

    added a thin client for followups

--- lib/plaid.rb
+++ lib/plaid.rb
@@ -4,10 +4,17 @@ module Plaid
       Plaid::Client::Base.configure(&block)
     end
 
+    # client for initializing access through Plaid
     def client(user, email, password, institution)
       Plaid::Client::Base.new(user, email, password, institution)
     end
 
+    # a special thin client for accessing Plaid without credentials securely
+    def thin_client(e_mail, institution, access_token)
+      Plaid::Client::ThinClient.new(e_mail, institution, access_token)
+    end
+
+    #scaffolding are general purpose tools for dealing with Plaid
     def scaffold
       Plaid::Scaffold::Base
     end
@@ -15,10 +22,12 @@ module Plaid
 end
 
 require 'plaid/client/client'
+require 'plaid/client/thin_client'
 require 'plaid/client/configuration'
 require 'plaid/client/connect'
 require 'plaid/client/entity'
 require 'plaid/client/balance'
+require 'plaid/client/followup'
 require 'plaid/scaffold/scaffold'
 require 'plaid/scaffold/institution'
 require 'plaid/plaid_object'
